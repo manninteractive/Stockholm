a8dd057f18864d9e5333e741e985879e102b68ef
sahilg <sahilg@usc.edu>
2013-11-21 17:19:52 -0800

    Send mail done for the stockholm theme
    Send mail done for the stockholm theme

--- mail.php
+++ mail.php
@@ -9,18 +9,15 @@
     <!-- Style sheets -->
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="css/font-awesome.css" rel="stylesheet" type="application/x-font-woff">
-    <link href="css/m-forms.min.css" rel="stylesheet">
-    <link href="css/m-buttons.css" rel="stylesheet">
+    
     <link href="css/custom.css" rel="stylesheet">
-    <link href="css/style-slider.css" rel="stylesheet">
-    <link href="css/custom-slider.css" rel="stylesheet">
+
     
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
 
 
 
-    <script type="text/javascript" src="js/modernizr.custom.79639.js"></script>
+
 
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
@@ -64,11 +61,7 @@
         </nav>
   </div>
 
-      <div class="pagination-centered mobilelogo hidden-desktop">                  
-            <a href="index.html">
-                <img src="img/logo_mobile.png" alt="">
-            </a>
-      </div>
+     
       
       
 <div class="container explain">
@@ -120,9 +113,9 @@ echo "</ul>";
 
 $msg = "From: " . clean_var($_REQUEST['name']) . "\n";
 $msg .= "Email: " . clean_var($_REQUEST['email']) . "\n";
-$header = 'From:'. clean_var($_REQUEST['email']);
+//$header = 'From:'. clean_var($_REQUEST['email']);
 
-mail($sendto, $subject, $msg, $header);
+mail($sendto, $subject, $msg);
 
 echo "<script type='text/javascript'>$(\".message\").fadeOut(\"slow\").fadeIn(\"slow\").animate({opacity: 1.0}, 5000).fadeOut(\"slow\");</script>";
 echo $thanks;
